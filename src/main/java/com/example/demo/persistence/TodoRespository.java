0dcd76acb4cca6a70550f183ddc50dc5560339af
kangilbin <kib3388@naver.com>
2021-12-29 23:06:50 +0900

    레포지터리 메서드->sql문으로 수정

--- src/main/java/com/example/demo/persistence/TodoRespository.java
+++ src/main/java/com/example/demo/persistence/TodoRespository.java
@@ -3,11 +3,14 @@ package com.example.demo.persistence;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.TodoEntity;
 
 @Repository
 public interface TodoRespository extends JpaRepository<TodoEntity, String>{
+	// ?1은 메서드의 매개변수의 순서 위치다.
+	@Query("select * from Todo t where t.userId=?1")
 	List<TodoEntity> findByUserId(String userId);
 }
