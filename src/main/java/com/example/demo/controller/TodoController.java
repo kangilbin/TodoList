bc8924bca19b1f9e472cf18fc14e1c325a0aaa8d
kangilbin <kib3388@naver.com>
2022-01-09 20:21:59 +0900

    패스워드 암호화 추가

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,9 +27,8 @@ public class TodoController {
 	private TodoService service;
 	
 	@PostMapping
-	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+	public ResponseEntity<?> createTodo(@AuthenticationPrincipal String userId,@RequestBody TodoDTO dto){
 		try {
-			String temporaryUserId = "temporary-user"; // temporary user id.
 			
 			// (1) TodoEntity로 변환한다.
 			TodoEntity entity = TodoDTO.toEntity(dto);
@@ -36,8 +36,8 @@ public class TodoController {
 			// (2) id를 null로 초기화한다. 생성 당시에는 id가 없어야하기 때문이다.
 			entity.setId(null);
 
-			// (3) 임시 사용자 아이디를 설정해 준다. 인증과 인가 기능이 없으므로 현재는 temporary-user만 로그인 없이 사용할 수 있는 애플리케이션이다.
-			entity.setUserId(temporaryUserId);
+			// (3) 임시 사용자 아이디를 설정해 준다. @AuthenticaionPrincipal에서 넘어온 userId로 설정
+			entity.setUserId(userId);
 			
 			// (4) 서비스를 이용해 Todo 엔티티를 생성한다.
 			List<TodoEntity> entities = service.create(entity);
@@ -59,11 +59,10 @@ public class TodoController {
 	}
 	
 	@GetMapping
-	public ResponseEntity<?> retrieveTodoList(){
-		String temporaryUserId = "temporary-user";
+	public ResponseEntity<?> retrieveTodoList(@AuthenticationPrincipal String userId){
 		
 		// (1) 서비스 메서드 retrieve() 메서드를 사용해 Todo 리스트를 가져온다.
-		List<TodoEntity> entities = service.retrieve(temporaryUserId);
+		List<TodoEntity> entities = service.retrieve(userId);
 		
 		// (2) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
 		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
@@ -76,14 +75,13 @@ public class TodoController {
 	}
 	
 	@PutMapping
-	public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto){
-		String temporaryUserId = "temporary-user";
+	public ResponseEntity<?> updateTodo(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
 		
 		// (1) dto를 entity로 변환한다.
 		TodoEntity entity = TodoDTO.toEntity(dto);
 		
-		// (2) id를 temporaryUserId로 초기화한다. 여기서 인증 추가
-		entity.setUserId(temporaryUserId);
+		// (2) id를 userId로 초기화한다. 여기서 인증 추가
+		entity.setUserId(userId);
 		
 		// (3) 서비스를 이용해 entity를 업데이트한다.
 		List<TodoEntity> entities = service.update(entity);
@@ -99,15 +97,14 @@ public class TodoController {
 	}
 	
 	@DeleteMapping
-	public ResponseEntity<?> deleteTodo(@RequestBody TodoDTO dto){
+	public ResponseEntity<?> deleteTodo(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
 		try {
-			String temporaryUserId = "temporary-user";
 			
 			// (1) TodoEntity로 변환한다.
 			TodoEntity entity = TodoDTO.toEntity(dto);
 			
-			// (2) 임시 사용자 아이디를 설정해 준다. 지금은 한 사용자만 로그인 없이 사용할 수 잇는 앱이다
-			entity.setUserId(temporaryUserId);
+			// (2) 임시 사용자 아이디를 설정해 준다.
+			entity.setUserId(userId);
 			
 			// (3) 서비스를 이용해 entity를 삭제한다.
 			List<TodoEntity> entities = service.delete(entity);
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,17 +2,17 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.model.UserEntity;
 import com.example.demo.security.TokenProvider;
 import com.example.demo.service.UserService;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -25,6 +25,9 @@ public class UserController {
 	
 	@Autowired
 	private TokenProvider tokenProvider;
+	
+	// Bean으로 작성해도 됨
+	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
 	@PostMapping("/signup")
 	public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
@@ -55,7 +58,7 @@ public class UserController {
 	
 	@PostMapping("/signin")
 	public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
-		UserEntity user = userService.getByCredntials(userDTO.getEmail(), userDTO.getPassword());
+		UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword(), passwordEncoder);
 		
 		if(user != null) {
 			// 토큰 생성
